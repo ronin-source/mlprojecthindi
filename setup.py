5502d27e668cc7562bc359117923ffd10043b860
ronin-source <roninbuilder@gmail.com>
2024-09-14 17:21:45 +0530

    project structure

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from setuptools import find_packages,setup
+from typing import List
 
 
+HYPEN_E_DOT='-e .'
 
-
-
-
-
-
-
-
-
-
-
+def get_requirements(file_path:str)->list[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"")for req in requiorements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 
 setup(
 name='mlproject',
 version='0.0.1',
 author='Ronin',
-author_email=''    
+author_email='roninbuilder@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt') 
 )
