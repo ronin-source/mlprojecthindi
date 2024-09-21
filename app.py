a6ad5d571cafcee6e31c08d19a76bbf90c07e8c6
ronin-source <roninbuilder@gmail.com>
2024-09-21 22:30:29 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from src.mlproject.logger import logging
-
-
+from src.logger import logging
+from src.exception import CustomException
+import sys
 
 if __name__=="__main__":
     logging.info("the execution has started")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("custom exception")
+        raise CustomException(e,sys)   
